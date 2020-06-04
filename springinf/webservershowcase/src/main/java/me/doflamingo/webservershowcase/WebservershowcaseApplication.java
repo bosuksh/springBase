a49284b6cbf3dbd89dba72953dde913eb5a3d665
Lee SangHyup <bosuksh@gmail.com>
2020-06-04 16:42:38 +0900

    [add] 스프링부트 내장 톰캣 예제

---
+++ springinf/webservershowcase/src/main/java/me/doflamingo/webservershowcase/WebservershowcaseApplication.java
@@ -0,0 +1,31 @@
+package me.doflamingo.webservershowcase;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebservershowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservershowcaseApplication.class, args);
+    }
+
+
+//    @Bean
+//    public ServletWebServerFactory serverFactory() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8080);
+//        return connector;
+//    }
+}
