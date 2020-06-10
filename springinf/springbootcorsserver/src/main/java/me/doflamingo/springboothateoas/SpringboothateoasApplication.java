12c978db2dcca18bac58cba1c609525bac9d4114
Lee SangHyup <bosuksh@gmail.com>
2020-06-11 01:32:46 +0900

    [add] spring boot crossorigin serverside

---
+++ springinf/springbootcorsserver/src/main/java/me/doflamingo/springboothateoas/SpringboothateoasApplication.java
@@ -0,0 +1,13 @@
+package me.doflamingo.springboothateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothateoasApplication.class, args);
+    }
+
+}
