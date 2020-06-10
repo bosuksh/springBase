d198d8270a4439f351f271d131ac316a552551d4
Lee SangHyup <bosuksh@gmail.com>
2020-06-11 01:44:20 +0900

    [add] spring boot cross origin client

---
+++ springinf/springbootcorsclient/src/main/java/me/doflamingo/springbootcorsclient/SpringbootcorsclientApplication.java
@@ -0,0 +1,13 @@
+package me.doflamingo.springbootcorsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcorsclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcorsclientApplication.class, args);
+    }
+
+}
